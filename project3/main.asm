314de97c20b19d1177e0314ffa6135ff027babb4
Leonardo dos Santos <leossb36@gmail.com>
2018-06-03 16:59:26 -0300

    Adding cube root calculation and error estimation
    Signed-off-by: Leonardo dos Santos <leossb36@gmail.com>
    Co-authored-by: Guilherme Guy de Andrade <guilhermeguy349@gmail.com>

--- project3/main.asm
+++ project3/main.asm
@@ -1,19 +1,32 @@
-#A raiz cubica é ZZ. O erro estimado eh de YY
+#A raiz cubica ? ZZ. O erro estimado eh de YY
 #the numb is (0 < x < 1) 
 
 .data
 	error: .asciiz "Out of Range"
-	
+
 	numb: .float 0.0  #the value
 	result_numb: .float 0.0 # the cubic root of 'numb'
-	estimate: .float 0.0  #estimate error
 	
-	constant1: .float 0.0
-	constant2: .float 1.0
-	constant3: .float 3.0
-	constant4: .float 2.0
-	
-	result1: .asciiz "A raiz cubica é "
+	step: .float 0.001f
+
+	lower_limit: .float 0.0
+    	lower_limit_cubed: .float 0.0f
+    
+   	upper_limit: .float 0.001
+	upper_limit_cubed: .float 0.000000001f
+    
+
+	root_aprox: .float 0.0
+   	root_aprox_cubed: .float 0.0
+
+	cubic_pow: .float 0.0 # numb ^ 3
+   	estimate: .float 0.0  #estimate error
+
+   	constant0: .float 0.0
+    	constant1: .float 1.0
+	constant2: .float 2.0
+
+	result1: .asciiz "A raiz cubica eh "
 	result2: .asciiz ". O erro estimado eh de "
 	result3: .asciiz "\n"
 .text
@@ -26,24 +39,25 @@ main:
 	lwc1 $f0, numb # pass n as arg check_float
 	jal check_float
 	jal calc_raiz
+	jal calc_erro
 	jal imprime_saida
 	jal exit_prog
 	
 	
 check_float:
 	# if (x > 0) return float
-	# if (x < 1) return float
+	# if (x <= 1) return float
 	
-	l.s $f1, constant1 # 0
-	l.s $f2, constant2 # 1
+	l.s $f1, constant0 # 0
+	l.s $f2, constant1 # 1
 	
 	greater_than_zero:
-		c.lt.s $f1,$f0 # check if x > 0 
+		c.lt.s $f1, $f0 # check if 0 < numb 
 		bc1t less_than_one # if true jump next test
 		bc1f not_in_range #if false jump not_in_range
 	
 	less_than_one:
-		c.lt.s $f0, $f2 # check if x < 1
+		c.le.s $f0, $f2 # check if x <= 1
 		bc1t in_range # if true jump in_range
 		bc1f not_in_range # if false jump not_in_range
 	
@@ -55,7 +69,6 @@ check_float:
 			j exit_prog
 	
 		in_range:
-			mov.s $f0, $f0
 			jr $ra #return var
 					
 	
@@ -66,36 +79,130 @@ le_float:
 	jr $ra
 
 calc_raiz:
+
 	# the reverse of power function is the root
 	# we need to do cubic root
-	# the reverse of cubic root is N ^ (1/3)
-	# 1/3 is constant
-		
-	l.s $f4, constant2 # 1.0 constant
-	l.s $f5, constant3 # 3.0 constant
-	lwc1 $f0, numb #imput number
-	l.s $f6, constant1 # i = 1 count
-	l.s $f3, constant4
+
+	# f0 = input number
+
+	# f2 = constant2 = 2.0
+
+	# f5 = lower_limit
+	# f6 = lower_limit_cubed
+
+	# f7 = upper_limit
+	# f8 = upper_limit_cubed
+
+	# f9 = root_aprox
+	# f10 = root_aprox_cubed
+    
+	# f4 = step
+
+	lwc1 $f0, numb
+
+	lwc1 $f2, constant2
 	
-	# for i = 0; i <= f3; i++
-	# 	x = (2x + n/(x^2)/3)
-		loop_exp:
-		mul.s $f1, $f0, $f0  # return numb = return numb * numb
-		mul.s $f3, $f0,  $f3 # 2 * numb
-		div.s $f8, $f4, $f1 # n/ x^2
-		add.s $f8, $f3, $f8
-		div.s $f8, $f8,$f5
-		
-		div.s $f2, $f4, $f6 # exp div = 1/3
-		add.s $f6, $f6, $f6 # i ++
-		c.lt.s $f6, $f5 # check if i <= 3
-		bc1f loop_exp # if count <= 3 return loop
-		
-		
-	s.s $f0, result_numb
+	lwc1 $f4, step # step = 0.001f
+
+	lwc1 $f5, lower_limit 
+	lwc1 $f6, lower_limit_cubed
+
+	lwc1 $f7, upper_limit 
+	lwc1 $f8, upper_limit_cubed
+
+	lwc1 $f9,  root_aprox 
+	lwc1 $f10, root_aprox_cubed
+
+  	# we want to find when lower_limit_cubed < lower_limit < upper_limit_cubed
+
+	# lower_limit_cubed < lower_limit && lower_limit < upper_limit_cubed
+	c.lt.s $f6, $f0 # lower_limit_cubed < numb
+	bc1f loop_find_limits
+	c.lt.s $f0, $f8 # numb < upper_limit_cubed
+	bc1t end_loop_find_limits
+
+	loop_find_limits:
+
+		mov.s $f5, $f7 # lower_limit = upper_limit
+		add.s $f7, $f7, $f4 # upper_limit += upper_limit
+
+		# lower_limit_cubed = lower_limit * lower_limit * lower_limit = lower_limit^3
+		mul.s $f6, $f5, $f5 
+		mul.s $f6, $f6, $f5
+
+		# upper_limit_cubed = upper_limit * upper_limit * upper_limit= upper_limit^3
+		mul.s $f8, $f7, $f7 
+		mul.s $f8, $f8, $f7
+
+		c.lt.s $f6, $f0 # lower_limit_cubed < numb
+		bc1f loop_find_limits
+		c.lt.s $f0, $f8 # numb < upper_limit_cubed
+		bc1f loop_find_limits
+
+	end_loop_find_limits:
+		# exit loop_find_limits
+
+		li $t1, 0 # t1 = i = 0
+		li $t2, 10 # t2 = iterations = 10
+
+	loop_find_root:
+		# for (int i = 0; i < iterations; i++)
 	
+		add.s $f9, $f5, $f7 # root_aprox = lower_limit + upper_limit
+		div.s $f9, $f9, $f2 # root_aprox = root_aprox / 2
+
+		# root_aprox_cubed = root_aprox * root_aprox * root_aprox = root_aprox^3
+		mul.s $f10, $f9, $f9
+		mul.s $f10, $f10, $f9
+
+		# if root_aprox_cubed < numb
+		c.lt.s $f10, $f0
+		bc1f elseif_find_root
+		mov.s $f5, $f9 # lower_limit = root_aprox
+		j end_elseif_find_root
+		
+		elseif_find_root:
+		# else if numb < root_aprox_cubed
+			c.lt.s $f0, $f10
+			bc1f end_elseif_find_root
+			mov.s $f7, $f9 # upper_limit = root_aprox
+
+		end_elseif_find_root:
+
+		addi $t1, $t1, 1 # i++
+		beq $t1, $t2, end_loop_find_root # if i == iterations then finish loop
+		
+	end_loop_find_root:
+
+	s.s $f9, result_numb
+
 	jr $ra
+    
+    
+cubic_power:
+	lwc1 $f2, numb
+
+	mul.s $f1, $f2, $f2
+	mul.s $f1, $f0, $f1 # cubic_numb = numb * numb *numb
+
+	jr $ra
+    
 calc_erro:
+	lwc1 $f0, numb
+	lwc1 $f1, result_numb
+	lwc1 $f2, estimate
+
+	# f3 = result_numb ^ 3
+	mul.s $f3, $f1, $f1
+	mul.s $f3, $f3, $f1 # cubic_numb = numb * numb *numb
+
+	# error = numb - (result_num ^ 3)
+	sub.s $f2, $f0, $f3
+	abs.s $f2, $f2
+
+	s.s $f2, estimate
+
+	jr $ra
 
 imprime_saida:
 	
